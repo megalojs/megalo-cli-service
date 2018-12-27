d4c672e56799d61936a67c69155087c6d1300d63
bigMeow <running@vip.163.com>
2018-12-27 23:30:01 +0800

    add config

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  extends: [
+    "plugin:vue-libs/recommended"
+  ],
+  plugins: [
+    "node"
+  ],
+  env: {
+    "jest": true
+  },
+  rules: {
+    "indent": ["error", 2, {
+      "MemberExpression": "off"
+    }]
+  },
+}
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # megalo-cli-service
-:hammer_and_wrench: megalo的开箱即用webpack小程序脚手架配置，内置megalo生态的部分插件，并提供用户自定义配置的入口
+:hammer_and_wrench: megalo的开箱即用webpack小程序脚手架配置，内置megalo生态的部分插件，并提供用户自定义配置的入口(megalo for vue-cli3插件出来之前的临时替代方案)
